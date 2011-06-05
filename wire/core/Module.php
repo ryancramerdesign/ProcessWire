209302b8a56655d9501b199dfdb9d71aab92c77a
Ryan Cramer <ryan@rc-d.net>
2011-06-05 07:57:14 -0400

    Some updates to the Module documentation in Module.php

--- wire/core/Module.php
+++ wire/core/Module.php
@@ -6,7 +6,7 @@
  * Provides the base interfaces required by modules. 
  * 
  * ProcessWire 2.x 
- * Copyright (C) 2010 by Ryan Cramer 
+ * Copyright (C) 2011 by Ryan Cramer 
  * Licensed under GNU/GPL v2, see LICENSE.TXT
  * 
  * http://www.processwire.com
@@ -68,7 +68,8 @@ interface Module {
 	 *
 	 * While the method is required, if you don't need it, then just leave the implementation blank.
 	 *
-	 * This is called after ProcessWire's API is fully ready for use and hooks
+	 * This is called after ProcessWire's API is fully ready for use and hooks. It is called at the end of the 
+	 * bootstrap process. This is before PW has started retrieving or rendering a page. 
 	 *
 	 */
 	public function init();
@@ -76,19 +77,36 @@ interface Module {
 	/**
 	 * Return an array of module information
 	 *
-	 * Array is associative with the following fields: 
-	 * - title: An alternate title, if you don't want to use the class name.
-	 * - version: an integer that indicates the version number, 101 = 1.0.1
-	 * - summary: a summary of the module (1 sentence to 1 paragraph max)
-	 * - permanent: boolean, true only if the module is permanent and uninstallable
-	 * - href: URL to more information about the module
-	 * - singular: is only one instance of this module allowed? return boolean true or false.
-	 * 	If specified, this overrides the isSingular() method, if that method exists in your class.
-	 * 	See the information for the isSingular() method for more about the 'singular' property.
-	 * - autoload: should this module load automatically at application start? return boolean true or false. 
-	 *	If specified, this overrides the isAutoload() method, if that method exists in your class. 
-	 *	See the information for the isAutoload() method for more about the 'autoload' property.
-	 * This method is OPTIONAL, which is why it's commented out below. 
+	 * Example:
+	 * 
+	 * 	public static function getModuleInfo() {
+	 * 		return array(
+	 * 			'title' => 'Your Module's Title',
+	 * 			'version' => 100,
+	 * 			'summary' => 'Description of what this module does and who made it.',
+	 * 			'href' => 'http://www.domain.com/info/about/this/module/', 
+	 * 			'singular' => false,
+	 *			'autoload' => false,
+	 * 			);
+	 * 	}
+	 * 
+	 *
+	 * The retured array is associative with the following fields. Fields bulleted with a "+" are required and 
+ 	 * fields bulleted with a "-" are optional, and fields bulleted with a "*" may be optional (see details): 
+	 * 
+	 * 	+ title: The module's title. 
+	 * 	+ version: an integer that indicates the version number, 101 = 1.0.1
+	 * 	+ summary: a summary of the module (1 sentence to 1 paragraph reommended)
+	 * 	- permanent: boolean - true only if the module is permanent and uninstallable (like for core PW modules)
+	 * 	- href: URL to more information about the module. 
+	 * 	* singular: is only one instance of this module allowed? return boolean true or false.
+	 * 		If specified, this overrides the isSingular() method, if that method exists in your class.
+	 * 		See the information for the isSingular() method for more about the 'singular' property.
+	 * 		If you don't provide an isSingular() method, then you should provide this property here. 
+	 * 	* autoload: should this module load automatically at application start? return boolean true or false. 
+	 *		If specified, this overrides the isAutoload() method, if that method exists in your class. 
+	 *		See the information for the isAutoload() method for more about the 'autoload' property.
+	 * 		If you don't provide an isAutoload() method, then you should provide this property here. 
 	 *
 	 * @return array
 	 *
@@ -101,13 +119,11 @@ interface Module {
 	 * If your Module descends from Wire, or any of it's derivatives (as would usually be the case), 
 	 * then you don't need to implement this method as it's already present. 
 	 *
-	 * If you do need to implement this, then it can be just: "return get_class($this);"
+	 * If you do want to implement this, then it can be just: "return get_class($this);"
 	 *
-	 * This method is required in this interface because ProcessWire may sometimes substitute placeholders
-	 * for a module that need to respond with the non-placeholder classname. PHP's get_class won't work,
-	 * which is why this method is required instead. 
-	 *
-	 * DEPRECATED / NO LONGER REQUIRED, EXCEPT BY ModulePlaceholder
+	 * ProcessWire may sometimes substitute placeholders for a module that need to respond with the 
+	 * non-placeholder classname. PHP's get_class won't work, which is why this method is used instead. 
+	 * This method is only required for PW internal classes. As a result, implementation is optional for others.
 	 *
 	public function className();
 	 */
@@ -115,8 +131,11 @@ interface Module {
 	/**
 	 * Perform any installation procedures specific to this module, if needed. 
 	 *
-	 * Note that the Modules class handles install/uninstall into the modules table. 
-	 * The Modules class calls this install method right before performing the install. 
+	 * The Modules class calls this install method right after performing the install. 
+	 * 
+	 * If this method throws an exception, PW will catch it, remove it from the installed module list, and
+	 * report that the module installation failed. You may specify details about why with the exception, i.e.
+	 * throw new WireException("Can't install because of ..."); 
 	 *
 	 * This method is OPTIONAL, which is why it's commented out below. 
 	 *
@@ -126,7 +145,6 @@ interface Module {
 	/**
 	 * Perform any uninstall procedures specific to this module, if needed. 
 	 *
-	 * Note that the Modules class handles the uninstall from the modules table. 
 	 * It calls this uninstall method right before completing the uninstall. 
 	 *
 	 * This method is OPTIONAL, which is why it's commented out below. 
@@ -139,19 +157,31 @@ interface Module {
 	 * Is this module intended to be only a single instance?
 	 *
 	 * This method is OPTIONAL and therefore commented out in this interface. If ommitted, it is assumed that
-	 * you are providing this inforamtion in the getModuleInfo() method. 
+	 * you are providing this inforamtion in the getModuleInfo() method, documented above.
 	 *
+	 * A module that returns TRUE is referred to as a "singular" module, because there will never be any more
+	 * than a single instance of the module running. 
+	 *
+	 * Why you'd use this method over the property in the getModuleInfo() method: 
 	 * Using this method is useful if you have multiple descending classes that you want to provide a default
 	 * value for, rather than having each descending class specify it individually in the getModuleInfo(). 
 	 * For example, Fieldtype or Inputfield modules. 
 	 *
-	 * Return true if this module is a single reusable instance, returning the same instance on every call from Modules. 
-	 * Return false if this module should return a new instance on every call from Modules.
 	 *
-	 * Modules that attach hooks should be singular. 
-	 * Modules that may have multiple instances (like Inputfields) should not be singular. 
+	 * Return TRUE if this module is a single reusable instance, returning the same instance on every call from Modules. 
+	 * Return FALSE if this module should return a new instance on every call from Modules.
+	 * 
+	 * 	- Singular modules will have their instance active for the entire request after instantiated. 
+	 * 	- Non-singular modules return a new instance on every $modules->get("YourModule") call. 
+	 * 	- Modules that attach hooks are usually singular. 
+	 * 	- Modules that may have multiple instances (like Inputfields) should NOT be singular. 
 	 *
+	 * If you are having trouble deciding whether to make your module singular or not, be sure to read the documentation
+	 * below for the isAutoload() method, because if your module is 'autoload' then it's probably also 'singular'.
+	 * 
 	 * @return bool
+	 * 
+	 * This method may be optional, which is why it's commented out below. 
 	 *
 	public function isSingular();
 	 */
@@ -160,14 +190,13 @@ interface Module {
 	 * Is this module automatically loaded at runtime?
 	 * 
 	 * This method is OPTIONAL and therefore commented out in this interface. If ommitted, it is assumed that
-	 * you are providing this inforamtion in the getModuleInfo() method. 
+	 * you are providing this information in the getModuleInfo() method. 
 	 *
-	 * Using this method is useful if you have multiple descending classes that you want to provide a default
-	 * value for, rather than having each descending class specify it individually in the getModuleInfo(). 
-	 * For example, Fieldtype or Inputfield modules. 
+	 * A module that returns TRUE is referred to as an "autoload" module, because it automatically loads as
+	 * part of ProcessWire's boot process. Autoload modules load before PW attempts to handle the web request.
 	 *
-	 * Return true if this module is automatically instantiated (and it's init method called) at runtime.
-	 * Return false if this module must be requested before it is loaded, instantiated, and init'd.  
+	 * Return TRUE if this module is automatically loaded [and it's init() method called] at runtime.
+	 * Return FALSE if this module must be requested [via the $modules->get('ModuleName') method] before it is loaded.
 	 *
 	 * Modules that are intended to attach hooks in the application typically should be autoloaded because
 	 * they listen in to classes rather than have classes call upon them. If they weren't autoloaded, then
@@ -175,6 +204,21 @@ interface Module {
 	 *
 	 * Modules that shouldn't be autoloaded are those that may or may not be needed at runtime, for example
 	 * Fieldtypes and Inputfields.
+	 *
+	 * As a side note, I can't think of any reason why a non-singular module would ever be autoload. The fact that
+	 * an autoload module is automatically loaded as part of PW's boot process implies it's probably going to be the
+ 	 * only instance running. So if you've decided to make your module 'autoload', then is safe to assume you've
+	 * also decided your module will also be singular (if that helps anyone reading this). 
+	 * 
+	 * Why you'd use this method over the property in the getModuleInfo() method: 
+	 * Using this method is useful if you have multiple descending classes that you want to provide a default
+	 * value for, rather than having each descending class specify it individually in the getModuleInfo(). 
+	 * For example, Fieldtype or Inputfield modules. 
+	 *
+	 *
+	 * @return bool
+	 *
+	 * This method may be optional, which is why it's commented out below. 
 	 *	
 	public function isAutoload(); 
 	 */ 
