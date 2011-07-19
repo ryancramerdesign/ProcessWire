0c965d78bdf130b3db086eb12caa01b92e68ff5f
Ryan Cramer <ryan@rc-d.net>
2011-07-19 14:36:52 -0400

    Added a basic file picker to PW/TinyMCE "link" modal window. @crashpaddesign

--- wire/modules/Process/ProcessPageEditLink/ProcessPageEditLink.js
+++ wire/modules/Process/ProcessPageEditLink/ProcessPageEditLink.js
@@ -11,14 +11,39 @@ $(document).ready(function() {
 		url: ''
 	};
 
+	var $fileSelect = $("#link_page_file"); 
+
+	function populateFileSelect(selectedPageData) {
+		// populate the files field
+		var $wrap = $("#wrap_link_page_file"); 
+		$.getJSON("./files?id=" + selectedPageData.id, function(data) {
+			$fileSelect.empty();	
+			$fileSelect.append("<option></option>"); 
+			$.each(data, function(key, val) {
+				var $option = $("<option value='" + key + "'>" + val + "</option>"); 		
+				$fileSelect.append($option);
+			});
+			$wrap.find("p.notes").text("Files on page " + selectedPageData.url);
+			$wrap.children().effect('highlight', {}, 500); 
+			$fileSelect.effect('bounce', {}, 50);
+		}); 
+	}
+
         $("#link_page_id").ProcessPageList(options).hide()
 		.bind('pageSelected', function(event, data) {
 			if(data.url.length) {
 				selectedPageData = data; 
 				selectedPageData.url = config.urls.root + data.url.substring(1); 
 				$("#link_page_url").val(selectedPageData.url); 
+				if($fileSelect.is(":visible")) populateFileSelect(selectedPageData);
 			}
 		}); 
+
+	$fileSelect.change(function() {
+		var $t = $(this);
+		var src = $t.val();
+		if(src.length) $("#link_page_url").val(src); 
+	}); 
 
 	$("#link_page_url").focus();
 
