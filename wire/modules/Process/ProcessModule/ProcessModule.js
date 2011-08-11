df422647907063d0203382cd686f18b2b7c4f785
Ryan Cramer <ryan@rc-d.net>
2011-08-11 11:18:57 -0400

    Various improvements to the admin Modules management.

--- wire/modules/Process/ProcessModule/ProcessModule.js
+++ wire/modules/Process/ProcessModule/ProcessModule.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
 	$(".ConfigurableModule").each(function() {
 		$(this).after($("<span class='ui-icon ui-icon-gear' style='float: right;'></span>").css('opacity', 0.5)); 
 	}); 
+	$(".not_installed").parent("a").css('opacity', 0.6).click(function() {
+		var id = $(this).children(".not_installed").attr('id');
+		if(confirm("Do you want to install " + $(this).text() + "?")) {
+			$("#install_" + id).click();
+		}
+		return false;
+	}); 
+	$("#wrap_reset").parent(".Inputfields").prependTo($("#modules_form")); 
 }); 
