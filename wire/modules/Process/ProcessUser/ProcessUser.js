bdfbf6a9fe70f0316691af207528a386eb09117b
Ryan Cramer <ryan@rc-d.net>
2011-08-17 10:35:42 -0400

    Minor JS tweaks to Role/User editor to hide unnecessary elements where applicable.

---
+++ wire/modules/Process/ProcessUser/ProcessUser.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	if($("#Inputfield_id").val() == 40) {
+		// guest user
+		// hide fields that aren't necessary ehre
+		$("#wrap_Inputfield_pass").hide(); 	
+		$("#wrap_Inputfield_email").hide();	
+		$("#wrap_Inputfield_roles input").attr('disabled', 'disabled');
+		$("#wrap_submit_save").remove();
+	}
+}); 
