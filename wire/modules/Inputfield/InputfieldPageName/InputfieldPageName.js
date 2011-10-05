41b58b5ef8a06bb8e1ece3f9206f4031344adec9
Ryan Cramer <ryan@rc-d.net>
2011-10-05 18:25:30 -0400

    Fix annoying cursor jump when editing an existing populated pageName field per somatonic issue #41.

--- wire/modules/Inputfield/InputfieldPageName/InputfieldPageName.js
+++ wire/modules/Inputfield/InputfieldPageName/InputfieldPageName.js
@@ -1,6 +1,10 @@
 jQuery(document).ready(function($) {
+	var lastVal = '';
 	$(".InputfieldPageName").find("input[type=text]").keyup(function() {
-		var val = $(this).val().replace(/[^-_a-z0-9.]/gi, '-').toLowerCase();
-		$(this).val(val).parent('p').siblings(".InputfieldPageNameURL").children("strong").text((val.length > 0 ? val + '/' : ''))
+		var value = $(this).val();
+		var val = value.replace(/[^-_a-z0-9.]/gi, '-').toLowerCase().replace(/--+/g, '-').replace(/^-|-$/g, ''); 
+		if(val != value) $(this).val(val);
+		if(val != lastVal) $(this).parent('p').siblings(".InputfieldPageNameURL").children("strong").text((val.length > 0 ? val + '/' : ''))
+		lastVal = val;
 	}).keyup();
 }); 
