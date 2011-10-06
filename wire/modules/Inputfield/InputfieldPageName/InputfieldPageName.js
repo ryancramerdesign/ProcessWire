7f14462e8fb4e001437a003a07d6e9c4f8c0ec5e
Ryan Cramer <ryan@rc-d.net>
2011-10-06 09:17:53 -0400

    Correction to the InputfieldPageName update to allow for leading/trailing dashes when typing

--- wire/modules/Inputfield/InputfieldPageName/InputfieldPageName.js
+++ wire/modules/Inputfield/InputfieldPageName/InputfieldPageName.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
 	var lastVal = '';
 	$(".InputfieldPageName").find("input[type=text]").keyup(function() {
 		var value = $(this).val();
-		var val = value.replace(/[^-_a-z0-9.]/gi, '-').toLowerCase().replace(/--+/g, '-').replace(/^-|-$/g, ''); 
+		var val = value.replace(/[^-_a-z0-9.]/gi, '-').toLowerCase().replace(/--+/g, '-'); 
 		if(val != value) $(this).val(val);
 		if(val != lastVal) $(this).parent('p').siblings(".InputfieldPageNameURL").children("strong").text((val.length > 0 ? val + '/' : ''))
 		lastVal = val;
