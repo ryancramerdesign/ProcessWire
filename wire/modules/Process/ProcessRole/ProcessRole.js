580565bef73803c32bfdaf265b904c7ac2d511d4
Ryan Cramer <ryan@rc-d.net>
2012-02-13 15:11:04 -0500

    Make the page-view permission already checked (@apeisa) in roles editor when adding a new role.

--- wire/modules/Process/ProcessRole/ProcessRole.js
+++ wire/modules/Process/ProcessRole/ProcessRole.js
@@ -1,23 +1,10 @@
 $(document).ready(function() {
-	/*
-	var filterInputfieldPermissions = function() {
-		var pageEdit = $("#Inputfield_permissions_32").is(":checked"); 
-		$("#wrap_Inputfield_permissions input").each(function() {
-			var val = $(this).val();
-			if(val == 36) return; // page-view
-			if(val == 32) return; // page-edit
-			if(val > 100) return; // non-system permission
-			var $li = $(this).parent('label').parent('li');
-			if(pageEdit) $li.show();
-				else $li.hide();
-		}); 
-	}; 
 
-	filterInputfieldPermissions();
-	$("#wrap_Inputfield_permissions input").click(filterInputfieldPermissions); 
-	*/
+	var $pageView = $("#Inputfield_permissions_36"); 
+
+	if(!$pageView.is(":checked")) $pageView.attr('checked', 'checked'); 
 	
-	$("#Inputfield_permissions_36").click(function() {
+	$pageView.click(function() {
 		if(!$(this).is(":checked")) {
 			$(this).attr('checked', 'checked');
 			alert('page-view is a required permission'); 
