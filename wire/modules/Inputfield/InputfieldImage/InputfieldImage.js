4183b3bed3fbf66138ef90b28800ccc3d3ebd921
Ryan Cramer <ryan@rc-d.net>
2012-03-22 13:14:30 -0400

    Add send-to-top/send-to-bottom links to Images input field.

--- wire/modules/Inputfield/InputfieldImage/InputfieldImage.js
+++ wire/modules/Inputfield/InputfieldImage/InputfieldImage.js
@@ -3,4 +3,19 @@ $(document).ready(function() {
 	$(".InputfieldImage .InputfieldFileList").live('AjaxUploadDone', function() {
 		$("a.InputfieldFileLink", $(this)).fancybox(); 
 	}); 
+
+	$(".InputfieldImage .InputfieldFileMove").live('click', function() {
+
+		var $li = $(this).parent('p').parent('li'); 
+		var $ul = $li.parent();
+
+		if($(this).is('.InputfieldFileMoveTop')) $ul.prepend($li); 
+			else $ul.append($li); 
+
+		$ul.children('li').each(function(n) {
+			$(this).find('.InputfieldFileSort').val(n); 	
+		}); 
+
+		return false;
+	}); 
 });
