1d2b19e803754eb78e797bfb3a740a71974340f4
Ryan Cramer <ryan@rc-d.net>
2012-06-29 12:14:13 -0400

    Add 'create page on enter' option to autocomplete inputfield. When using the PageAutocomplete Inputfield and the add-pages option is enabled, you can now have the autocomplete add your input as a new page if it doesn't match anything, just by hitting enter. To me, this is much more natural behavior here than clicking 'create new item' and entering them in the textarea.

--- wire/modules/Inputfield/InputfieldPageAutocomplete/InputfieldPageAutocomplete.js
+++ wire/modules/Inputfield/InputfieldPageAutocomplete/InputfieldPageAutocomplete.js
@@ -22,8 +22,15 @@ var InputfieldPageAutocomplete = {
 		var $ol = $('#' + id + '_items'); 
 		var $input = $('#' + id + '_input'); 
 		var $icon = $input.parent().find(".InputfieldPageAutocompleteStatus");
+		var $note = $input.parent().find(".InputfieldPageAutocompleteNote"); 
+		var numAdded = 0; // counter that keeps track of quantity items added
+		var numFound = 0; // indicating number of pages matching during last ajax request
 
 		$icon.click(function() { $input.focus(); }); 
+
+		function isAddAllowed() {
+			return $('#_' + $ol.attr('data-name') + '_add_items').size() > 0; 
+		}
 
 		$input.autocomplete({
 			minLength: 2,
@@ -34,9 +41,15 @@ var InputfieldPageAutocomplete = {
 				$.getJSON(url + '&' + searchField + operator + escape(request.term), function(data) { 
 
 					$icon.removeClass('ui-icon-refresh'); 
+					numFound = data.total;
 
 					if(data.total > 0) {
 						$icon.addClass('ui-icon-arrowreturn-1-s'); 
+
+					} else if(isAddAllowed()) {
+						$icon.addClass('ui-icon-plus'); 
+						$note.show();
+
 					} else {
 						$icon.addClass('ui-icon-alert'); 
 					}
@@ -60,9 +73,33 @@ var InputfieldPageAutocomplete = {
 
 		}).blur(function() {
 			if(!$(this).val().length) $input.val('');
-			$icon.removeClass('ui-icon-arrowreturn-1-s ui-icon-alert'); 
+			$icon.removeClass('ui-icon-arrowreturn-1-s ui-icon-alert ui-icon-plus'); 
+			$note.hide();
+
 		}).keyup(function() {
-			$icon.removeClass('ui-icon-arrowreturn-1-s ui-icon-alert'); 
+			$icon.removeClass('ui-icon-arrowreturn-1-s ui-icon-alert ui-icon-plus');
+
+		}).keydown(function(event) {
+			if(event.keyCode == 13) {
+				// prevents enter from submitting the form
+				event.preventDefault();
+				// instead we add the text entered as a new item
+				// if there is an .InputfieldPageAdd sibling, which indicates support for this
+				if(isAddAllowed()) { 
+					if($.trim($input.val()).length < 1) {
+						$input.blur();
+						return false;
+					}
+					numAdded++;
+					// new items have a negative page_id
+					var page = { page_id: (-1 * numAdded), label: $input.val() }; 
+					// add it to the list
+					InputfieldPageAutocomplete.pageSelected($ol, page); 
+					$input.val('').blur().focus();
+					$note.hide();
+				}
+				return false;
+			}
 		}); 
 
 		var makeSortable = function($ol) { 
@@ -126,15 +163,25 @@ var InputfieldPageAutocomplete = {
 	 *
 	 */
 	rebuildInput: function($ol) {
-		var id = $ol.attr('id');
-		id = id.substring(0, id.lastIndexOf('_')); 
+		var id = $ol.attr('data-id');
+		var name = $ol.attr('data-name');
+		//id = id.substring(0, id.lastIndexOf('_')); 
 		var $input = $('#' + id);
 		var value = '';
+		var addValue = '';
 		$ol.children(':not(.itemTemplate)').each(function() {
-			if(value.length > 0) value += ',';
-			value += $(this).children('.itemValue').text();
+			var v = parseInt($(this).children('.itemValue').text());
+			if(v > 0) {
+				value += ',' + v; 
+			} else if(v < 0) {
+				value += ',' + v; 
+				addValue += $(this).children('.itemLabel').text() + "\n";
+			}
 		}); 
 		$input.val(value);
+
+		var $addItems = $('#_' + name + '_add_items'); 
+		if($addItems.size() > 0) $addItems.val(addValue);
 	}
 
 
