8afaa59c0a5d6b813533717e933516f99f457261
Ryan Cramer <ryan@rc-d.net>
2012-09-20 10:52:02 -0400

    Update version number to 2.2.8

--- wire/core/ProcessWire.php
+++ wire/core/ProcessWire.php
@@ -35,7 +35,7 @@ class ProcessWire extends Wire {
 
 	const versionMajor = 2; 
 	const versionMinor = 2; 
-	const versionRevision = 7; 
+	const versionRevision = 8; 
 
 	/**
 	 * Given a Config object, instantiates ProcessWire and it's API
