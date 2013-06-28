316f96484d129fba1fc3e58da8db9cbb90b1a1cc
Ryan Cramer <ryan@rc-d.net>
2013-06-28 13:34:49 -0400

    Update InputfieldPageName so that it converts apostrophes and quotes to blank rather than hyphens.

--- wire/modules/Inputfield/InputfieldPageName/InputfieldPageName.js
+++ wire/modules/Inputfield/InputfieldPageName/InputfieldPageName.js
@@ -15,6 +15,9 @@ var InputfieldPageName = {
 			}
 		}
 
+        // replace all types of quotes with nothing
+        name = name.replace(/['"\u0022\u0027\u00AB\u00BB\u2018\u2019\u201A\u201B\u201C\u201D\u201E\u201F\u2039\u203A\u300C\u300D\u300E\u300F\u301D\u301E\u301F\uFE41\uFE42\uFE43\uFE44\uFF02\uFF07\uFF62\uFF63]/g, '');
+        
 		// replace invalid with dash
 		name = name.replace(/[^-_.a-z0-9 ]/g, '-');
 	
