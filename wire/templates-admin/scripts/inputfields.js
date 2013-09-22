ac7c6be0f62baac856daf981d57832245f1fe608
Ryan Cramer <ryan@rc-d.net>
2013-09-22 10:26:14 -0400

    Update to Inputfield dependencies attempting to correct condition when both showIf and requiredIf are used on the same field for the same conditions. This version also adds management of column height to hopefully normalize the height of columns in a row.

--- wire/templates-admin/scripts/inputfields.js
+++ wire/templates-admin/scripts/inputfields.js
@@ -7,17 +7,23 @@
  *
  */
 
+
 function consoleLog(note) {
 	// uncomment the line below to enable debugging console
-	// console.log(note);
+	console.log(note);
 }
+
 
 /**
  * Inputfield Depedendencies
  *
  */
+var InputfieldDependenciesProcessing = false;
 
 function InputfieldDependencies() {
+
+	if(InputfieldDependenciesProcessing) return;
+	InputfieldDependenciesProcessing = true; 
 
 	$(".InputfieldStateShowIf, .InputfieldStateRequiredIf").each(function() {
 
@@ -85,6 +91,7 @@ function InputfieldDependencies() {
 		 *
 		 */
 		function inputfieldChange() {
+			InputfieldDependenciesProcessing = true; 
 
 			consoleLog('-------------------------------------------------------------------');
 			consoleLog('Field "' + $fieldToShow.attr('id') + '" detected a change to a dependency field! Beginning dependency checks...');
@@ -92,8 +99,10 @@ function InputfieldDependencies() {
 			// number of changes that were actually made to field visibility
 			var numVisibilityChanges = 0;
 
-			// quantity of matched conditions
-			var show = 0;
+			var totalMatched = 0;
+			var show = true; 
+			var requiredMatches = 0; 
+			var notRequiredMatches = 0;
 
 			for(var c = 0; c < conditions.length; c++) {
 
@@ -186,43 +195,63 @@ function InputfieldDependencies() {
 
 				consoleLog('----');
 
-				// if at least one value matched, then increment or 'show' value
-				if(matched > 0) show++;
-			}
-
-			consoleLog('Summary (required/matched): ' + conditions.length + ' / ' + show);
-
-			// determine whether to show or hide the field
-			if(show > 0 && conditions.length == show) {
-				// show it
-				consoleLog('Determined that field "' + fieldNameToShow + '" should be visible.');
+				// determine whether to show or hide the field
 				if(condition.type == 'show') {
-					if($fieldToShow.is('.InputfieldStateHidden')) {
-						// field is hidden so show/fade in
-						$fieldToShow.removeClass('InputfieldStateHidden').fadeIn();
-						numVisibilityChanges++;
-						consoleLog('Field is now visible.');
+					if(matched > 0) {
+						// show it, which is the default behavior
 					} else {
-						consoleLog('Field is already visible.');
+						show = false;
 					}
 
 				} else if(condition.type == 'required') {
-					$fieldToShow.addClass('InputfieldStateRequired').find(":input:visible[type!=hidden]").addClass('required'); // may need to focus a specific input?
+					if(matched > 0) {
+						// make it required it
+						requiredMatches++;
+					} else {
+						notRequiredMatches++;
+					}
+				}
+
+
+			} // foreach(conditions)
+
+			// consoleLog('Summary (required/matched): ' + conditions.length + ' / ' + show);
+
+			var required = requiredMatches > 0 && notRequiredMatches == 0; 
+
+			if(show) {
+				consoleLog('Determined that field "' + fieldNameToShow + '" should be visible.');
+				if($fieldToShow.is('.InputfieldStateHidden')) {
+					// field is hidden so show/fade in
+					$fieldToShow.removeClass('InputfieldStateHidden').fadeIn();
+					numVisibilityChanges++;
+					consoleLog('Field is now visible.');
+				} else {
+					consoleLog('Field is already visible.');
 				}
 			} else {
 				consoleLog('Determined that field "' + fieldNameToShow + '" should be hidden.');
 				// hide it
-				if(condition.type == 'show') {
-					if(!$fieldToShow.is('.InputfieldStateHidden')) {
-						$fieldToShow.addClass('InputfieldStateHidden').hide();
-						consoleLog('Field is now hidden.');
-						numVisibilityChanges++;
-					} else {
-						consoleLog('Field is already hidden.');
-					}
-				} else if(condition.type == 'required') {
-					$fieldToShow.removeClass('InputfieldStateRequired').find(":input.required").removeClass('required');
+				if(!$fieldToShow.is('.InputfieldStateHidden')) {
+					$fieldToShow.addClass('InputfieldStateHidden').hide();
+					consoleLog('Field is now hidden.');
+					numVisibilityChanges++;
+				} else {
+					consoleLog('Field is already hidden.');
 				}
+				if(required) {
+					consoleLog('Field is required but cancelling that since it is not visible.'); 
+					required = false;
+				}
+			}
+
+			if(required && requiredMatches > 0) {
+				consoleLog('Determined that field "' + fieldNameToShow + '" should be required.');
+				$fieldToShow.addClass('InputfieldStateRequired').find(":input:visible[type!=hidden]").addClass('required'); // may need to focus a specific input?
+
+			} else if(!required && notRequiredMatches > 0) {
+				consoleLog('Determined that field "' + fieldNameToShow + '" should not be required.');
+				$fieldToShow.removeClass('InputfieldStateRequired').find(":input.required").removeClass('required');
 			}
 
 			if(numVisibilityChanges > 0) {
@@ -230,6 +259,8 @@ function InputfieldDependencies() {
 				InputfieldColumnWidths();
 				$(window).resize(); // trigger for FormBuilder or similar
 			}
+
+			InputfieldDependenciesProcessing = false;
 		}; // END inputfieldChange()
 
 		/***************************************************************************************************************
@@ -311,12 +342,13 @@ function InputfieldDependencies() {
 				// attach change event to dependency inputfield
 				$inputfield.change(inputfieldChange);
 
-				// run the event for the first time to initalize the field
-				inputfieldChange();
 			}
 		}
+		// run the event for the first time to initalize the field
+		inputfieldChange();
 	});
 
+	InputfieldDependenciesProcessing = false;
 }
 
 /**
@@ -325,6 +357,8 @@ function InputfieldDependencies() {
  */
 
 function InputfieldColumnWidths() {
+
+	var colspacing = null; 
 
 	/**
 	 * Return the current with of $item based on its "style" attribute
@@ -370,12 +404,34 @@ function InputfieldColumnWidths() {
 		consoleLog('InputfieldColumnWidths.setWidth(' + $item.attr('id') + ': ' + pct + '%');
 	}
 
+	function setHeight($item, maxColHeight) {
+		var h = $item.height();
+		if(h == maxColHeight) return;
+		if($item.hasClass('InputfieldStateCollapsed')) return;
+		var pad = maxColHeight-h; 
+		if(pad < 0) pad = 0;
+		var $container = $item.children('.ui-widget-content'); 
+		if(pad == 0) {
+			// do nothing, already the right height
+		} else {
+			consoleLog('Adjusting ' + $item.attr('id') + ' from ' + h + ' to ' + maxColHeight); 
+			var $spacer = $("<div class='maxColHeightSpacer'></div>");
+			$container.append($spacer); 
+			$spacer.height(pad); 
+		}
+	}
+
 	// for columns that don't have specific widths defined, add the InputfieldColumnWidthFirst
 	// class to them which more easily enables us to exclude them from our operations below
 	$(".Inputfield:not(.InputfieldColumnWidth)").addClass(".InputfieldColumnWidthFirst");
 
 	// cycle through all first columns in a multi-column row
 	$(".InputfieldColumnWidthFirst.InputfieldColumnWidth").each(function() {
+
+		if(colspacing === null) {
+			colspacing = $(this).parents('form').attr('data-colspacing'); 
+			if(typeof colspacing == 'undefined') colspacing = 1; 
+		}
 
 		var $firstItem = $(this);
 
@@ -390,13 +446,24 @@ function InputfieldColumnWidths() {
 		var itemWidth = $item == null ? 0 : rowWidth;
 		var numItems = $items.size();
 
-		if($firstItem.is(".InputfieldStateHidden")) numItems--;
+		if($firstItem.is(".InputfieldStateHidden")) {
+			numItems--;
+			// item that leads the list, even though it may not be the first (first could be hidden)
+			var $leadItem = $items.eq(0);
+		} else {
+			// lead item is first item
+			var $leadItem = $firstItem; 
+		}
+
+		// remove any spacers already present for adjusting height
+		$leadItem.find(".maxColHeightSpacer").remove();
+		$items.find(".maxColHeightSpacer").remove();
 
 		// subtract the quantity of items from the maxRowWidth since each item has a 1% margin
-		var maxRowWidth = 100 - numItems;
+		var maxRowWidth = 100 - (numItems * colspacing);
 
-		// item that leads the list, even though it may not be the first (first could be hidden)
-		var $leadItem = $items.eq(0);
+		// keep track of the max column height
+		var maxColHeight = $leadItem.height(); 
 
 		// if our temporary class is in any of the items, remove it
 		$items.removeClass("InputfieldColumnWidthFirstTmp");
@@ -407,7 +474,15 @@ function InputfieldColumnWidths() {
 			$item = $(this);
 			itemWidth = getWidth($item);
 			rowWidth += itemWidth;
+			var h = $item.height();
+			if(h > maxColHeight) maxColHeight = h; 
 		});
+
+		// ensure that all columns in the same row share the same height
+		if(maxColHeight > 0) {
+			setHeight($leadItem, maxColHeight); 
+			$items.each(function() { setHeight($(this), maxColHeight); }); 
+		}
 
 		// if the current rowWidth is less than the full width, expand the last item as needed to fill the row
 		if(rowWidth < maxRowWidth) {
@@ -465,6 +540,7 @@ function InputfieldStates() {
 			$(this).children('span.ui-icon').toggleClass('ui-icon-triangle-1-e ui-icon-triangle-1-s');
 
 			if($.effects && $.effects['highlight']) $li.children('.ui-widget-header').effect('highlight', {}, 300);
+			setTimeout('InputfieldColumnWidths()', 500); 
 			return false;
 		})
 
@@ -476,8 +552,24 @@ function InputfieldStates() {
 
 /*********************************************************************************************/
 
+var InputfieldWindowResizeQueued = false;
+
+function InputfieldWindowResizeActions() {
+	consoleLog('InputfieldWindowResizeActions()'); 
+	InputfieldColumnWidths(); 
+	InputfieldWindowResizeQueued = false;
+}
+
 $(document).ready(function() {
+
 	InputfieldStates();
 	InputfieldDependencies();
 	InputfieldColumnWidths();
+
+	$(window).resize(function() {
+		if(InputfieldWindowResizeQueued) return;
+		InputfieldWindowResizeQueued = true; 
+		setTimeout('InputfieldWindowResizeActions()', 2000); 	
+	}); 
+
 }); 
