06f9d3648e05241621574976557bf31f8bf41f27
Ryan Cramer <ryan@rc-d.net>
2014-01-12 12:15:52 -0500

    UI enhancements to ProcessModule and add support for module deletion.

--- wire/modules/Process/ProcessModule/ProcessModule.js
+++ wire/modules/Process/ProcessModule/ProcessModule.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 
+	/*
 	$(".ConfigurableModule").each(function() {
 		if($(this).parent().is('.not_installed')) return;
 		$(this).after($("<i class='fa fa-cog' style='float: right; margin-top: 3px;'></i>")); 
 	}); 
+	*/
 
 	$(".not_installed").parent("a").css('opacity', 0.6).click(function() {
 
@@ -19,6 +21,12 @@ $(document).ready(function() {
 
 		return false;
 	});
+
+	$("button.ProcessModuleSettings").click(function() {
+		console.log('click'); 
+		var $a = $(this).parents('tr').find('.ConfigurableModule').parent('a');
+		window.location.href = $a.attr('href'); 
+	}); 
 
     if($('#modules_form').size() > 0) {
         $('#modules_form').WireTabs({
