6777581ab007da8a52e2e72330cd0fa68de4a697
Ryan Cramer <ryan@rc-d.net>
2014-02-04 10:54:33 -0500

    Add @teppokoivula's suggested change to allow for InputfieldPage's findPagesCode option to return a single page in addition to the existing PageArray. When a single Page is returned, it is assumed to be a Parent, and the selectable pages become its children.

--- wire/core/ProcessWire.php
+++ wire/core/ProcessWire.php
@@ -33,7 +33,7 @@ class ProcessWire extends Wire {
 
 	const versionMajor = 2; 
 	const versionMinor = 3; 
-	const versionRevision = 15; 
+	const versionRevision = 16; 
 	
 	const statusBoot = 0; // system is booting
 	const statusInit = 2; // system and modules are initializing
