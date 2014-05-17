c54a8e41bfe67201625c2b2dbc9b4da7ee34aa6a
Ryan Cramer <ryan@rc-d.net>
2014-05-17 06:28:01 -0400

    Add @horst-n PR #475, plus a few other minor adjustments

--- wire/core/Pageimage.php
+++ wire/core/Pageimage.php
@@ -196,6 +196,14 @@ class Pageimage extends Pagefile {
 	 */
 	protected function ___size($width, $height, $options) {
 
+		// I was getting unnecessarily resized images without this code below,
+		// but this may be better solved in ImageSizer?
+		$w = $this->width();
+		$h = $this->height();
+		if($w == $width && $h == $height) return $this; 
+		if(!$height && $w == $width) return $this; 
+		if(!$width && $h == $height) return $this; 
+
 		if(!is_array($options)) { 
 			if(is_string($options)) {
 				// optionally allow a string to be specified with crop direction, for shorter syntax
