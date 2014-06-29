7007a4fcb024f198d3faa9637663982f81462f33
Emanuel <kraemerseele@gmx.de>
2014-06-29 13:59:36 +0200

    Switched to more generic selector; better handling of multiple occurrences

--- wire/modules/Inputfield/InputfieldCheckboxes/InputfieldCheckboxes.js
+++ wire/modules/Inputfield/InputfieldCheckboxes/InputfieldCheckboxes.js
@@ -1,26 +1,57 @@
 jQuery(function ($) {
 
-  var $selectAll = $('.InputfieldCheckboxesSelectAll a'),
-      $removeField = $('[name^="remove_fields"]'),
-      allChecked = false;
+  /**
+   * Iterating over all occurrences of the input field
+   * on the page.
+   */
+  $('.InputfieldCheckboxesSelectAll').each(function () {
 
-  $selectAll.on('click', function () {
-    allChecked = !allChecked;
-    $removeField.attr('checked', allChecked);
-    $(this).text(allChecked ? InputfieldCheckboxesConfig.deSelectAll : InputfieldCheckboxesConfig.selectAll);
-  });
+    var $selectAll = $(this).find('a'),
+        $checkboxes = $('#' + this.id.replace('-select-all', '')).find('input[type="checkbox"]'),
+        allChecked = false;
 
-  $removeField.on('change', function () {
-    var allCheckedLen = $.grep($removeField, function (item) {
-      return item.checked;
-    }).length;
-    if ( allCheckedLen === $removeField.length ) {
-      allChecked = true;
-      $selectAll.text(InputfieldCheckboxesConfig.deSelectAll);
-      return;
+    /**
+     * Changes all enabled checkboxes.
+     * 
+     * @return {boolean} True, if at least one checkbox changed
+     */
+    function unCheckAll() {
+      var changed = 0;
+      $.each($checkboxes, function () {
+        if ( !this.disabled ) {
+          this.checked = !allChecked;
+          changed += 1;
+        }
+      });
+      return !!changed;
     }
-    allChecked = false;
-    $selectAll.text(InputfieldCheckboxesConfig.selectAll);
-  })
+
+    /**
+     * Handling '(De-) Select all'-clicks
+     */
+    $selectAll.on('click', function () {
+      if ( unCheckAll() ) {
+        allChecked = !allChecked;
+        $selectAll.text(allChecked ? InputfieldCheckboxesConfig.deSelectAll : InputfieldCheckboxesConfig.selectAll);
+      }
+    });
+
+    /**
+     * Handling checkbox state changes.
+     */
+    $checkboxes.on('change', function () {
+      var allCheckedLen = $.grep($checkboxes, function (item) {
+        return item.checked;
+      }).length;
+      if ( allCheckedLen === $checkboxes.length ) {
+        allChecked = true;
+        $selectAll.text(InputfieldCheckboxesConfig.deSelectAll);
+        return;
+      }
+      allChecked = false;
+      $selectAll.text(InputfieldCheckboxesConfig.selectAll);
+    });
+
+  });
 
 });
