a499d74fbdadc4bb80a390188ad78ca6490bf9d4
Camilo Castro <camilo@cervezapps.cl>
2014-08-06 19:24:18 -0400

    Added status codes property

--- wire/config.php
+++ wire/config.php
@@ -173,6 +173,61 @@ $config->pagefileSecurePathPrefix = '.';
 $config->pagefileExtendedPaths = false;
 
 /**
+ * statusCodes: array of extention to status-code header, 
+ * based on the Lime Project
+ * https://github.com/aheinze/Lime/
+ * Copyright (c) 2014 Artur Heinze
+ *
+ */
+$config->statusCodes = array(
+       // Informational 1xx
+       100 => 'Continue',
+       101 => 'Switching Protocols',
+       // Successful 2xx
+       200 => 'OK',
+       201 => 'Created',
+       202 => 'Accepted',
+       203 => 'Non-Authoritative Information',
+       204 => 'No Content',
+       205 => 'Reset Content',
+       206 => 'Partial Content',
+       // Redirection 3xx
+       300 => 'Multiple Choices',
+       301 => 'Moved Permanently',
+       302 => 'Found',
+       303 => 'See Other',
+       304 => 'Not Modified',
+       305 => 'Use Proxy',
+       307 => 'Temporary Redirect',
+       // Client Error 4xx
+       400 => 'Bad Request',
+       401 => 'Unauthorized',
+       402 => 'Payment Required',
+       403 => 'Forbidden',
+       404 => 'Not Found',
+       405 => 'Method Not Allowed',
+       406 => 'Not Acceptable',
+       407 => 'Proxy Authentication Required',
+       408 => 'Request Timeout',
+       409 => 'Conflict',
+       410 => 'Gone',
+       411 => 'Length Required',
+       412 => 'Precondition Failed',
+       413 => 'Request Entity Too Large',
+       414 => 'Request-URI Too Long',
+       415 => 'Unsupported Media Type',
+       416 => 'Request Range Not Satisfiable',
+       417 => 'Expectation Failed',
+       // Server Error 5xx
+       500 => 'Internal Server Error',
+       501 => 'Not Implemented',
+       502 => 'Bad Gateway',
+       503 => 'Service Unavailable',
+       504 => 'Gateway Timeout',
+       505 => 'HTTP Version Not Supported'
+);
+
+/**
  * fileContentTypes: array of extention to content-type header, used by file passthru functions.
  *
  * Any content types that should be force-download should be preceded with a plus sign.
