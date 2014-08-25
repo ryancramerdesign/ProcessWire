576f103afa45969bbb7e09d64044bfde0b5e9243
Ryan Cramer <ryan@rc-d.net>
2014-08-25 14:59:54 -0400

    Adjustment to system update #6 per issue #615

--- wire/modules/System/SystemUpdater/SystemUpdate6.php
+++ wire/modules/System/SystemUpdater/SystemUpdate6.php
@@ -10,14 +10,16 @@ class SystemUpdate6 extends SystemUpdate {
 		$query = $this->wire('database')->prepare("SHOW tables LIKE 'caches'"); 
 		$query->execute();
 		if($query->rowCount() > 0) return true; 
+		$dbEngine = $this->wire('config')->dbEngine;
+		$dbCharset = $this->wire('config')->dbCharset; 
 		
 		$sql = "
 			CREATE TABLE caches (
-				name VARCHAR(255) NOT NULL PRIMARY KEY,
+				name VARCHAR(128) NOT NULL PRIMARY KEY,
 				data MEDIUMTEXT NOT NULL, 
 				expires DATETIME NOT NULL, 
 				INDEX expires (expires)
-			);
+			) ENGINE=$dbEngine DEFAULT CHARSET=$dbCharset;
 			";
 	
 		try {
