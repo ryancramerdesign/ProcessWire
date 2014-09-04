23624b4d592ed3b643e0d500ff7ba0931753040c
Ryan Cramer <ryan@rc-d.net>
2014-09-04 12:09:27 -0400

    Fix site-blank issue #634

--- site-blank/templates/basic-page.php
+++ site-blank/templates/basic-page.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<title><?php echo $title; ?></title>
+		<title><?php echo $page->title; ?></title>
 		<link rel="stylesheet" type="text/css" href="<?php echo $config->urls->templates?>styles/main.css" />
 	</head>
 	<body>
