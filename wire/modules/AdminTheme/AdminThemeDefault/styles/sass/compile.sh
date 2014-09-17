d4b1b1a4992cf49e5c25dc85b07f58999b3f4eb4
Ryan Cramer <ryan@rc-d.net>
2014-09-17 13:16:33 -0400

    Add compile shell scripts for the sass files for each admin theme

---
+++ wire/modules/AdminTheme/AdminThemeDefault/styles/sass/compile.sh
@@ -0,0 +1,4 @@
+sass --style=compressed main-warm.scss:../main-warm.css
+sass --style=compressed main-classic.scss:../main-classic.css
+sass --style=compressed main-futura.scss:../main-futura.css
+sass --style=compressed main-modern.scss:../main-modern.css
