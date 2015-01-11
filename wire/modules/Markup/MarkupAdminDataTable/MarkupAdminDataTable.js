6ecf35f29f684dc3b084c9d65d5ef73bad8c375d
Andreas Wahlstrom <andreas.wahlstrom@tv4.se>
2015-01-11 15:42:26 +0100

    hold shift to select multiple checkboxes in tables
    makes batch processing easier.

--- wire/modules/Markup/MarkupAdminDataTable/MarkupAdminDataTable.js
+++ wire/modules/Markup/MarkupAdminDataTable/MarkupAdminDataTable.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-	$("table.AdminDataTableSortable").tablesorter();
+    $table = $("table.AdminDataTableSortable")
+    $table.tablesorter();
+
+    var lastChecked = null;
+    $table.find('input[type=checkbox]').click(function(e) {
+        var $checkboxes = $table.find('input[type=checkbox]');
+        if(!lastChecked) {
+            lastChecked = this;
+            return;
+        }
+        if(e.shiftKey) {
+            var start = $checkboxes.index(this);
+            var end = $checkboxes.index(lastChecked);
+            $checkboxes.slice(Math.min(start,end), Math.max(start,end)+ 1).attr('checked', lastChecked.checked);
+        }
+        lastChecked = this;
+    });
 }); 
