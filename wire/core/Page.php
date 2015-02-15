2ad1023d04f66f5f53176d762054c554da898863
Ryan Cramer <ryan@rc-d.net>
2015-02-15 12:04:12 -0500

    Optimizations to FieldtypeFloat/FieldtypeInteger sanitization of unrecognized values

--- wire/core/Page.php
+++ wire/core/Page.php
@@ -23,11 +23,11 @@
  * @property string $path The page's URL path from the homepage (i.e. /about/staff/ryan/)
  * @property string $url The page's URL path from the server's document root (may be the same as the $page->path)
  * @property string $httpUrl Same as $page->url, except includes protocol (http or https) and hostname.
- * @property Page $parent The parent Page object or a NullPage if there is no parent.
+ * @property Page|string|int $parent The parent Page object or a NullPage if there is no parent. For assignment, you may also use the parent path (string) or id (integer). 
  * @property int $parent_id The numbered ID of the parent page or 0 if homepage or NullPage.
  * @property PageArray $parents All the parent pages down to the root (homepage). Returns a PageArray.
  * @property Page $rootParent The parent page closest to the homepage (typically used for identifying a section)
- * @property Template $template The Template object this page is using
+ * @property Template|string $template The Template object this page is using. The template name (string) may also be used for assignment.
  * @property FieldsArray $fields All the Fields assigned to this page (via it's template, same as $page->template->fields). Returns a FieldsArray.
  * @property int $numChildren The number of children (subpages) this page has, with no exclusions (fast).
  * @property int $numVisibleChildren The number of visible children (subpages) this page has. Excludes unpublished, no-access, hidden, etc.
