fb5a4ca9e0cfab18ff30561820efff811e88c477
Hari KT <kthari85@gmail.com>
2015-06-27 15:46:04 +0530

    Make sure the cookie is set secure if config https is true

--- index.php
+++ index.php
@@ -177,7 +177,8 @@ function ProcessWireBootConfig() {
 	ini_set('session.use_cookies', true); 
 	ini_set('session.use_only_cookies', 1);
 	ini_set('session.cookie_httponly', 1); 
-	ini_set('session.gc_maxlifetime', $config->sessionExpireSeconds); 
+	ini_set('session.gc_maxlifetime', $config->sessionExpireSeconds);
+	ini_set('session.cookie_secure', (bool) $config->https); 
 	
 	if(ini_get('session.save_handler') == 'files') {
 		if(ini_get('session.gc_probability') == 0) {
