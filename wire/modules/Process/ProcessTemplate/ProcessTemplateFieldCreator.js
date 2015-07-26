1d3745944765842cb3a7d1aed02c9a83dcfd53a8
Ryan Cramer <ryan@rc-d.net>
2015-07-26 05:55:39 -0400

    Add @adrianbj fix to ProcessTemplateFieldCreator.js per issue #1217

--- wire/modules/Process/ProcessTemplate/ProcessTemplateFieldCreator.js
+++ wire/modules/Process/ProcessTemplate/ProcessTemplateFieldCreator.js
@@ -3,7 +3,7 @@ function TemplateFieldAddDialog() {
 
     var $a = $(this);
     var closeOnSave = false;
-    var $iframe = pwModalWindow($a.attr('href'), {}, 'large'); 
+    var $iframe = pwModalWindow($a.attr('href'), {}, 'large');
 
     $iframe.load(function() {
 
@@ -30,18 +30,17 @@ function TemplateFieldAddDialog() {
                     'click': function() {
                         $button.click();
                         if(closeOnSave) setTimeout(function() {
-                            var newFieldId = $("iframe").contents().find("#Inputfield_id:last").val();
+                            var newFieldId = $icontents.find("#Inputfield_id:last").val();
                             $iframe.dialog('close');
-
                             var numOptions = $('#fieldgroup_fields option').size();
 
-                            $("#fieldgroup_fields option").eq(1).before($("<option></option>").val(newFieldId).text($("iframe").contents().find("#Inputfield_name").val()));
+                            $("#fieldgroup_fields option").eq(1).before($("<option></option>").val(newFieldId).text($icontents.find("#Inputfield_name").val()));
                             $('#fieldgroup_fields option[value="'+newFieldId+'"]')
                                 .attr('id', 'asm0option'+numOptions)
-                                .attr('data-desc', ($("iframe").contents().find("#field_label").val()))
-                                .attr('data-status', ($("iframe").contents().find("#Inputfield_type option:selected").text()));
+                                .attr('data-desc', ($icontents.find("#field_label").val()))
+                                .attr('data-status', ($icontents.find("#Inputfield_type option:selected").text()));
 
-                            $("#asmSelect0 option").eq(1).before($("<option></option>").val(newFieldId).text($("iframe").contents().find("#Inputfield_name").val()));
+                            $("#asmSelect0 option").eq(1).before($("<option></option>").val(newFieldId).text($icontents.find("#Inputfield_name").val()));
                             $("#asmSelect0").find('option:selected').removeAttr("selected");
                             $('#asmSelect0 option[value="'+newFieldId+'"]')
                                 .attr('rel', 'asm0option'+numOptions)
@@ -50,9 +49,6 @@ function TemplateFieldAddDialog() {
                                 .attr('disabled', 'disabled')
                                 .trigger('change')
                                 .removeAttr("selected");
-
-                            $("iframe").remove(); // clear iframe from dom as each new field was getting the same ID as the previous one
-
                         }, 500);
                         closeOnSave = true;
                     }
@@ -62,7 +58,7 @@ function TemplateFieldAddDialog() {
             $button.hide();
         });
 
-        $iframe.setButtons(buttons); 
+        $iframe.setButtons(buttons);
     });
 
     return false;
