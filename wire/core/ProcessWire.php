51f04ba6ba630065fa0b79d0a2a91d7d9d32ccfb
Ryan Cramer <ryan@rc-d.net>
2015-10-02 12:36:51 -0400

    Optimizations to dependency support in InputfieldForm.module, and bump version number to 2.6.19.

--- wire/core/ProcessWire.php
+++ wire/core/ProcessWire.php
@@ -35,7 +35,7 @@ class ProcessWire extends Wire {
 
 	const versionMajor = 2; 
 	const versionMinor = 6; 
-	const versionRevision = 18; 
+	const versionRevision = 19; 
 	const versionSuffix = 'dev';
 	
 	const indexVersion = 250; // required version for index.php file (represented by PROCESSWIRE define)
