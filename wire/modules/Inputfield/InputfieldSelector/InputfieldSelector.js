e13faedc6ea798a30d5741caafe322946e5e524e
Ryan Cramer <ryan@rc-d.net>
2015-11-01 06:24:21 -0500

    Attempted fixes to InputfieldSelector for issues mentioned by Macrura when used within PageLister

--- wire/modules/Inputfield/InputfieldSelector/InputfieldSelector.js
+++ wire/modules/Inputfield/InputfieldSelector/InputfieldSelector.js
@@ -94,9 +94,10 @@ var InputfieldSelector = {
 				var $ac = $row.find(".input-value-autocomplete"); 
 				if($ac.length > 0) {
 					// setup autocomplete
-					var field = $row.find(".select-field").val();
+					var subfield = $row.find(".select-subfield");
+					var field = subfield.length ? subfield.val() : $row.find(".select-field").val();
 					var name = $row.parents(".InputfieldSelector").find("input.selector-value").attr('name'); // selector-value intentional!
-					InputfieldSelector.setupAutocomplete($ac, field, name); 
+					InputfieldSelector.setupAutocomplete($ac, field, name);
 				}
 			}); 
 		}
