eaaef229416519eb8bf554ee1f24069ed7ba820b
Ryan Cramer <ryan@rc-d.net>
2015-11-15 09:02:04 -0500

    Follow-up fix to issue #1236 with ProcessProfile/ProcessUser when combined with Firefox and password fields where FF ignores autocomplete attribute.

--- wire/modules/Process/ProcessProfile/ProcessProfile.js
+++ wire/modules/Process/ProcessProfile/ProcessProfile.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
-	// from @horst-n:
+	// from @horst-n #1236:
 	// prevent browser supported autocomplete for password fields (e.g. on Profilepage)
 	// to force this, attribute autocomplete='off' needs to be set for the password field
-	if ($(".FieldtypePassword[autocomplete='off']")) {
+	if($(".FieldtypePassword[autocomplete='off']").length) {
 		// simply set the value empty on document.ready doesn't work in FireFox,
 		// but one second later, it works :)
 		setTimeout(function() {
-			$(".FieldtypePassword[autocomplete='off']").attr('value', '');
+			$(".FieldtypePassword[autocomplete='off']").attr('value', '')
+				.closest('.Inputfield').removeClass('InputfieldStateChanged'); // @GerardLuskin
 		}, 1000);
 	}
 }); 
