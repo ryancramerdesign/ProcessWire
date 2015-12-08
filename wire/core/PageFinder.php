8d6cf7aef7461dda473f6f91079ca95e082435b0
Ryan Cramer <ryan@rc-d.net>
2015-12-08 06:03:58 -0500

    Fix issue #1547 adding support for sorting by non-FieldtypePage fieldtypes that use Page or PageArray, like FieldtypeTable and FieldtypeRepeater

--- wire/core/PageFinder.php
+++ wire/core/PageFinder.php
@@ -1059,6 +1059,7 @@ class PageFinder extends Wire {
 				$subValue = $database->escapeCol($subValue);
 				$tableAlias = "_sort_$fieldName". ($subValue ? "_$subValue" : '');
 				$table = $database->escapeTable($field->table);
+				$blankValue = $field->type->getBlankValue(new NullPage(), $field);
 
 				$query->leftjoin("$table AS $tableAlias ON $tableAlias.pages_id=pages.id");
 
@@ -1066,7 +1067,8 @@ class PageFinder extends Wire {
 					// sort by quantity of items
 					$value = "COUNT($tableAlias.data)";
 
-				} else if($field->type instanceof FieldtypePage) {
+				// } else if($field->type instanceof FieldtypePage) {
+				} else if(is_object($blankValue) && ($blankValue instanceof PageArray || $blankValue instanceof Page)) {
 					// If it's a FieldtypePage, then data isn't worth sorting on because it just contains an ID to the page
 					// so we also join the page and sort on it's name instead of the field's "data" field.
 					if(!$subValue) $subValue = 'name';
