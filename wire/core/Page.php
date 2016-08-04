6ccce2a35f71243832486def61e00fdf146999d1
Ryan Cramer <ryan@rc-d.net>
2016-08-04 06:15:21 -0400

    Add Page::getInputfield($fieldName) method per LostKobrakai request issue #1950

--- wire/core/Page.php
+++ wire/core/Page.php
@@ -2754,6 +2754,9 @@ class Page extends WireData implements \Countable, WireMatchable {
 	 * required to edit this page. You may also specify a `$fieldName` argument to limit what is contained
 	 * in the returned InputfieldWrapper. 
 	 * 
+	 * Please note this method deals only with custom fields, not system fields name 'name' or 'status', etc., 
+	 * as those are exclusive to the ProcessPageEdit page editor. 
+	 * 
 	 * #pw-advanced
 	 * 
 	 * @param string $fieldName Optional field to limit to, typically the name of a fieldset or tab.
@@ -2761,7 +2764,43 @@ class Page extends WireData implements \Countable, WireMatchable {
 	 *
 	 */
 	public function getInputfields($fieldName = '') {
-		return $this->template ? $this->template->fieldgroup->getPageInputfields($this, '', $fieldName) : null;
+		$of = $this->of();
+		if($of) $this->of(false);
+		$wrapper = $this->template ? $this->template->fieldgroup->getPageInputfields($this, '', $fieldName) : null;
+		if($of) $this->of(true);
+		return $wrapper;
+	}
+
+	/**
+	 * Get a single Inputfield for the given field name
+	 * 
+	 * - If requested field name refers to a single field, an Inputfield object is returned. 
+	 * - If requested field name refers to a fieldset or tab, then an InputfieldWrapper representing will be returned.
+	 * - Returned Inputfield already has values populated to it.
+	 * - Please note this method deals only with custom fields, not system fields name 'name' or 'status', etc., 
+	 *   as those are exclusive to the ProcessPageEdit page editor. 
+	 * 
+	 * #pw-advanced
+	 * 
+	 * @param string $fieldName
+	 * @return Inputfield|InputfieldWrapper|null Returns Inputfield, or null if given field name doesn't match field for this page.
+	 * 
+	 */
+	public function getInputfield($fieldName) {
+		$inputfields = $this->getInputfields($fieldName);
+		if($inputfields) {
+			$field = $this->wire('fields')->get($fieldName);
+			if($field && $field instanceof FieldtypeFieldsetOpen) {
+				// requested field name is a fieldset, returns InputfieldWrapper
+				return $inputfields;
+			} else {
+				// requested field name is a single field, return Inputfield
+				return $inputfields->children()->first();
+			}
+		} else {
+			// requested field name is not applicable to this page
+			return null;
+		}
 	}
 
 	/**
