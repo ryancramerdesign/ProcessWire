82f87ebcc8e2e88ec3746c6e68b2061f2c0bb0a2
Jonas Hamm <contact@jonashamm.de>
2016-08-26 16:05:43 +0200

    changing charset configuration in all templates to simpler equivalent

--- wire/modules/AdminTheme/AdminThemeReno/default.php
+++ wire/modules/AdminTheme/AdminThemeReno/default.php
@@ -47,7 +47,7 @@ $extras = $adminTheme->getExtraMarkup();
 <html class="<?php echo $helpers->renderBodyClass(); ?>" lang="<?php echo $helpers->_('en'); 
 	/* this intentionally on a separate line */ ?>">
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta charset="utf-8" />
 	<meta name="robots" content="noindex, nofollow" />
 	<meta name="google" content="notranslate" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
