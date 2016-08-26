8b91bf9e717332469397b38d26bbe6bb0fd8b3b5
Jonas Hamm <contact@jonashamm.de>
2016-08-26 16:08:40 +0200

    changing charset configuration in all templates to simpler equivalent in dev branch

--- wire/modules/Markup/MarkupHTMLPurifier/htmlpurifier/HTMLPurifier.standalone.php
+++ wire/modules/Markup/MarkupHTMLPurifier/htmlpurifier/HTMLPurifier.standalone.php
@@ -18647,7 +18647,7 @@ class HTMLPurifier_Lexer_DOMLex extends HTMLPurifier_Lexer
         }
 
         $ret .= '<html><head>';
-        $ret .= '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
+        $ret .= '<meta charset="utf-8" />';
         // No protection if $html contains a stray </div>!
         $ret .= '</head><body><div>' . $html . '</div></body></html>';
         return $ret;
